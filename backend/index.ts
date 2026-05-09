44d1393b60a41f03a0be8ecf41bbd3a8ff636c9d
Ankit Pandit <ankitkumarpandit1221@gmail.com>
2026-05-09 14:08:05 +0530

    fix: update CORS config to handle preflight request itself

--- backend/index.ts
+++ backend/index.ts
@@ -33,10 +33,10 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   }),
 );
-
-app.options("*", cors());
 
 // Body parsers 
 app.use(express.json());
